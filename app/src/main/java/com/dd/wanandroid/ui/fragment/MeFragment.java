ad63bf0fb3e0ebbe5104af58e1a722ef685a5896
daidong <ddongisme@gmail.com>
2019-06-02 22:42:26 +0800

    update something

--- app/src/main/java/com/dd/wanandroid/ui/fragment/MeFragment.java
+++ app/src/main/java/com/dd/wanandroid/ui/fragment/MeFragment.java
@@ -3,6 +3,7 @@ package com.dd.wanandroid.ui.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,7 +27,7 @@ public class MeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@Nullable LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me, container, false);
     }
